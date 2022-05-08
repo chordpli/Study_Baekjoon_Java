ef38cc3b0ecfe44c0894034cd33f9bf5b356036a
Junho Kim <chordplaylist@gmail.com>
2022-05-08 23:43:29 +0900

    22_05_08 23:43

--- src/study01/check/check01.java
+++ src/study01/check/check01.java
@@ -3,16 +3,11 @@ package study01.check;
 import java.util.Scanner;
 public class check01 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] remove = new String[] {"C","A","M","B","R","I","D","G","E"};
-        String str = sc.next();
-        String[] strArr = str.split("");
-        for(int j=0; j<strArr.length; j++) {
-            for(int i=0; i<remove.length; i++) {
-                strArr[j] = strArr[j].replace(remove[i],"");
-            }
-            System.out.print(strArr[j]);
-        }
-        sc.close();
+        String[] arr = new String[2];
+        arr[0] = "abc";
+        String a = "abc";
+        String b = "bcd";
+
+        System.out.println(arr[0].charAt(1) == b.charAt(0));
     }
 }
--- src/study04/Number_1969.java
+++ src/study04/Number_1969.java
@@ -1,4 +1,60 @@
 package study04;
 
+import java.util.Scanner;
+
 public class Number_1969 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //DNA수
+        int number = Integer.parseInt(sc.next());
+        //입력된 DNA 길이
+        int len = Integer.parseInt(sc.next());
+
+        sc.nextLine();
+
+        String[] DNA = new String[number];
+
+        for (int i = 0; i < number; i++) {
+            DNA[i] = sc.nextLine();
+        }
+        int sumHD = 0;
+        StringBuilder sd = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            int T = 0, A = 0, G = 0, C = 0, max = 0;
+            int HD = 0;
+
+            for (int j = 0; j < number; j++) {
+                if (DNA[j].charAt(i) == 'A') {
+                    A++;
+                } else if (DNA[j].charAt(i) == 'T') {
+                    T++;
+                } else if (DNA[j].charAt(i) == 'G') {
+                    G++;
+                } else if (DNA[j].charAt(i) == 'C') {
+                    C++;
+                }
+            }
+            if (A >= T && A >= G && A >= C) {
+                max = A;
+                sd.append("A");
+            } else if (C >= A && C >= G && C >= T) {
+                max = C;
+                sd.append("C");
+            } else if (G >= A && G >= C && G >= T) {
+                max = G;
+                sd.append("G");
+            } else {
+                max = T;
+                sd.append("T");
+            }
+            HD = number - max;
+            sumHD += HD;
+        }
+
+        System.out.println(sd);
+        System.out.println(sumHD);
+        sc.close();
+    }
 }
