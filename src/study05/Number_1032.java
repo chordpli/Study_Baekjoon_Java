f43256a39a81032dd4252c8c831bac055fa1554a
Junho Kim <chordplaylist@gmail.com>
2022-05-11 23:35:19 +0900

    22_05_11 23:35

--- src/study05/Number_1032.java
+++ src/study05/Number_1032.java
@@ -19,6 +19,11 @@ public class Number_1032 {
         for (int i = 1; i < files.length; i++) {
             for (int j = 0; j < files[0].length(); j++) {
                 if (files[0].charAt(j) != files[i].charAt(j)) {
+                    // substring은 문자열을 자르는 함수
+                    // substring(j+1)은 j+1번째에 위치한 문자열을 포함하여 그 이후로 다 리턴.
+                    // SubString(0,j)는 0에서부터 j번째 앞 부분까지의 값을 리턴
+                    // 즉 j번째의 문자를 바꾸기 원하므로
+                    // j번째 앞에서 문자를 자르고 + '?' + 원하는 문자를 작성 후 이어 붙이는 방법.
                     files[0] = files[0].substring(0, j) + '?' + files[0].substring(j + 1);
                 }
             }
