074386b6d0e697c7eb618e89adde59abe93bf6c8
Junho Kim <chordplaylist@gmail.com>
2022-05-21 23:44:52 +0900

    22_05_21 23:44

---
+++ src/study08/Number_10610.java
@@ -0,0 +1,25 @@
+package study08;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Number_10610 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] sArr = input.split("");
+
+        Arrays.sort(sArr);
+
+        
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
