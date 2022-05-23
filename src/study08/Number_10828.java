88c7f854144a13311b12036cff702e673e7cf768
Junho Kim <chordplaylist@gmail.com>
2022-05-23 23:26:39 +0900

    22_05_23 23:26

---
+++ src/study08/Number_10828.java
@@ -0,0 +1,19 @@
+package study08;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Number_10828 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+
+
+
+
+    }
+
+}
