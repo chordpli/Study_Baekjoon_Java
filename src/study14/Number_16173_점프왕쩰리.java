eb6b21ffa0271f4a3dac2ae3c1539d629b6e049f
Junho Kim <chordplaylist@gmail.com>
2022-06-12 01:37:08 +0900

    22_06_12 01:34 메모리초과

--- src/study14/Number_16173_점프왕쩰리.java
+++ src/study14/Number_16173_점프왕쩰리.java
@@ -1,30 +1,44 @@
 package study14;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Number_16173_점프왕쩰리 {
     static int[][] map;
+    static int N;
+    static String result = "Hing";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
         map = new int[N][N];
 
-        for(int i = 0; i < N; i ++){
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; i <N; j++){
-                map[i][N] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                map[j][i] = Integer.parseInt(st.nextToken());
             }
         }
 
+        br.close();
+        bw.write(search(0, 0));
+        bw.flush();
+        bw.close();
     }
 
-    public int search(int num){
-
-        if(map[])
-
+    public static String search(int x, int y) {
+        if (map[x][y] == -1) {
+            return result = "HaruHaru";
+        } else {
+            if (x + map[x][y] < N) {
+                search(x + map[x][y], y);
+            }
+            if (y + map[x][y] < N) {
+                search(x, y + map[x][y]);
+            }
+        }
+        return result;
     }
 }
