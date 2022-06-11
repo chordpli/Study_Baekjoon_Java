1b2b6b344506a42d41b6c441af8b5755f7b6d12d
Junho Kim <chordplaylist@gmail.com>
2022-06-12 01:36:52 +0900

    22_06_12 01:34 메모리초과 해결

---
+++ src/study14/Number_16173_점프왕쩰리2.java
@@ -0,0 +1,47 @@
+package study14;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Number_16173_점프왕쩰리2 {
+    static int[][] map;
+    static boolean[][] visited = new boolean[3][3];
+    static int N;
+    static String result = "Hing";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[j][i] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        br.close();
+        bw.write(search(0, 0));
+        bw.flush();
+        bw.close();
+    }
+
+    public static String search(int x, int y) {
+        visited[x][y] = true;
+
+        if (map[x][y] == -1) {
+            return result = "HaruHaru";
+        } else {
+            if (x + map[x][y] < N && !visited[x + map[x][y]][y]) {
+                search(x + map[x][y], y);
+            }
+            if (y + map[x][y] < N && !visited[x][y + map[x][y]]) {
+                search(x, y + map[x][y]);
+            }
+        }
+        return result;
+    }
+}
