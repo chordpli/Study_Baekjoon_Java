d68e719685fe5d75a1f802189303a1eafe30b4d3
Junho Kim <chordplaylist@gmail.com>
2022-06-14 23:50:59 +0900

    22_06_14 23:50

--- src/study15/Number_11047_동전0.java
+++ src/study15/Number_11047_동전0.java
@@ -6,33 +6,29 @@ public class Number_11047_동전0 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
         int N = Integer.parseInt(sc.next());
         int K = sc.nextInt();
         int count = 0;
         int money = 0;
         int[] coins = new int[N];
 
-        for (int i = N-1 ; i >= 0; i--){
+        for (int i = N - 1; i >= 0; i--) {
             coins[i] = sc.nextInt();
         }
 
-        while(money != K){
+        while (money != K) {
 
-            for(int i = 0; i < N; i++){
-                if(money == K){
-                    i=N-1;
+            for (int i = 0; i < N; i++) {
+                if (money == K) {
+                    i = N - 1;
                 }
-                if(coins[i] <= K && money+coins[i] <= K){
+                if (coins[i] <= K && money + coins[i] <= K) {
                     money += coins[i];
                     count++;
                     i--;
                 }
             }
         }
-
         System.out.println(count);
-
-
     }
 }
